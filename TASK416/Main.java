0e93bbed3616fd81b2b7be833109c1bde208d49a
salwa-said <salwwa2000@gmail.com>
2026-08-11 15:24:09 +0400

    comlpeted task 416

---
+++ TASK416/Main.java
@@ -0,0 +1,26 @@
+package TASK416;
+
+public class Main {
+    public static void main() {
+        Book b = new Book();
+        Book a = new Book();
+        Book c = new Book();
+
+
+
+        b.title="Java Basics";
+        b.author="Ishaq";
+        b.printInfo();
+
+
+        a.title="Inheritance Basics";
+        a.author="Fatma";
+        a.printInfo();
+
+        c.title="Polymorphism Basics";
+        c.author="Salim";
+        c.printInfo();
+
+
+    }
+}
